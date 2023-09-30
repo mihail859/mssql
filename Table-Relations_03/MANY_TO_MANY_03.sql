71b133907d3ee95dc9c52a6e3834f964a46940d5
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-09-30 16:38:31 +0300

    update

---
+++ Table-Relations_03/MANY_TO_MANY_03.sql
@@ -0,0 +1,38 @@
+CREATE TABLE [Students](
+	[StudentID] INT PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE [Exams](
+	[ExamID] INT PRIMARY KEY IDENTITY(101, 1),
+	[Name] NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE [StudentsExams](
+	[StudentID] INT,
+	[ExamID] INT,
+	PRIMARY KEY([StudentID], [ExamID]),
+	FOREIGN KEY([StudentID]) REFERENCES [Students]([StudentID]),
+	FOREIGN KEY([ExamID]) REFERENCES [Exams]([ExamID])
+)
+INSERT INTO [Students]([Name])
+VALUES
+('Mila'),
+('Toni'),
+('Ron')
+
+INSERT INTO [Exams]([Name])
+VALUES
+('SpringMVC'),
+('Neo4j'),
+('Oracle 11g')
+
+INSERT INTO 
+[StudentsExams]([StudentID], [ExamID])
+VALUES
+(1, 101), 
+(1, 102), 
+(2, 101), 
+(3, 103), 
+(2, 102), 
+(2, 103)
