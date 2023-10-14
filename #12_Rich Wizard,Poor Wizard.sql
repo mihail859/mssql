2270f36e8b80922a7da96a4afef6991cb98bfb21
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-10-14 13:57:41 +0300

    Add files via upload

---
+++ #12_Rich Wizard,Poor Wizard.sql
@@ -0,0 +1,10 @@
+SELECT
+	SUM(QueryDIFFERENCE.[Difference])
+		FROM 
+		(SELECT
+			FirstName AS [Host Wizard],
+			DepositAmount AS [Host Wizard Deposit],
+			LEAD(FirstName) OVER(ORDER BY Id) AS [Guest Wizard],
+			LEAD(DepositAmount) OVER(ORDER BY Id) AS [Guest Wizard Deposit],
+			DepositAmount - LEAD(DepositAmount) OVER(ORDER BY Id) AS [Difference]
+		FROM WizzardDeposits) AS QueryDIFFERENCE
